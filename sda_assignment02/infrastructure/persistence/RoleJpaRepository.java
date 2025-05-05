a2b19f16f0fb6910fac6ab903889e2ed04b4fa39
Badhon1401 <140379134+Badhon1401@users.noreply.github.com>
2025-05-05 08:36:58 +0600

    Delete sda_assignment02 directory

--- sda_assignment02/infrastructure/persistence/RoleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bsse1401.sda_assignment01.infrastructure.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RoleJpaRepository extends JpaRepository<RoleJpaEntity, UUID> {}
--- sda_assignment02/infrastructure/persistence/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bsse1401.sda_assignment01.infrastructure.persistence;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-
-public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {}
-
